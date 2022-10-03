beeb3d1956c7637b74d60d268423531f11d3bcf2
zeya khan <zeya.khan@techchefz.com>
2022-10-03 22:20:17 +0530

    logo change

--- components/logo/index.js
+++ components/logo/index.js
@@ -2,7 +2,7 @@ import {Icon} from "../../tailwind";
 const index = ()=>{
     const design = (
         <>
-            <h1 className="text-red-500 font-bold text-2xl">StreamWap</h1>
+            <h1 className="text-red-500 font-bold text-2xl">streamkro</h1>
         </>
     );
     return design;
